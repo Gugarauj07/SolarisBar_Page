c08b5a104666114ee3b5c5449dc7a7351f49c405
gugarauj07 <gugaraujo01@gmail.com>
2024-03-21 11:16:34 -0400

    fixes

--- assets/js/main.js
+++ assets/js/main.js
@@ -103,8 +103,8 @@ themeButton.addEventListener('click', () => {
 const sr = ScrollReveal({
     origin: 'top',
     distance: '60px',
-    duration: 2500,
-    delay: 400,
+    duration: 1500,
+    delay: 1,
     // reset: true, Animations repeat
 })
 
