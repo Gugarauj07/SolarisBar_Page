3b2d8a986bbc595794226d71246c2e4ae2c91130
gugarauj07 <gugaraujo01@gmail.com>
2024-03-22 11:09:14 -0400

    filtering days

--- assets/js/main.js
+++ assets/js/main.js
@@ -114,3 +114,66 @@ sr.reveal(`.home__data`, {origin: 'bottom'})
 sr.reveal(`.about__data, .recently__data`, {origin: 'left'})
 sr.reveal(`.about__img, .recently__img`, {origin: 'right'})
 sr.reveal(`.popular__card`, {interval: 100})
+
+/*=============== SwitchButton ===============*/
+
+document.addEventListener('DOMContentLoaded', function() {
+    var mixer = mixitup('#menu-dish');
+});
+
+jQuery(".menu-toggle").click(function () {
+    jQuery(".main-navigation").toggleClass("toggled");
+});
+
+jQuery(window).on('load', function () {
+    $('body').removeClass('body-fixed');
+
+    //activating tab of filter
+    let targets = document.querySelectorAll(".filter");
+    let activeTab = 0;
+    let old = 0;
+    let dur = 0.4;
+    let animation;
+
+    for (let i = 0; i < targets.length; i++) {
+        targets[i].index = i;
+        targets[i].addEventListener("click", moveBar);
+    }
+
+    // initial position on first === All 
+    gsap.set(".filter-active", {
+        x: targets[0].offsetLeft,
+        width: targets[0].offsetWidth
+    });
+
+    function moveBar() {
+        if (this.index != activeTab) {
+            if (animation && animation.isActive()) {
+                animation.progress(1);
+            }
+            animation = gsap.timeline({
+                defaults: {
+                    duration: 0.4
+                }
+            });
+            old = activeTab;
+            activeTab = this.index;
+            animation.to(".filter-active", {
+                x: targets[activeTab].offsetLeft,
+                width: targets[activeTab].offsetWidth
+            });
+
+            animation.to(targets[old], {
+                color: "#0d0d25",
+                ease: "none"
+            }, 0);
+            animation.to(targets[activeTab], {
+                color: "#fff",
+                ease: "none"
+            }, 0);
+
+        }
+
+    }
+
+});
